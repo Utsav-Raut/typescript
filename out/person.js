a6ecfab91461972c7d280ea4ecc1dcac26e7157c
Utsav Raut <utsav.raut@yahoo.com>
2018-07-10 09:26:56 +0530

    first change

---
+++ out/person.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    return Person;
+}());
+exports.Person = Person;
+;
+//tsc test.ts --out output_script.js --watch
+//tsc -help
+//npm install lodash --save
+//npm install @types/lodash --save-dev
---
+++ person.ts
@@ -0,0 +1,10 @@
+export class Person{
+    "lastName": string;
+    "firstName": string;
+};
+
+
+//tsc test.ts --out output_script.js --watch
+//tsc -help
+//npm install lodash --save
+//npm install @types/lodash --save-dev
